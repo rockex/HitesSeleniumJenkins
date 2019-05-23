377739116335176d99019690c39bd384f4cfe7bd
kiko <kiko@KONOHA>
2019-05-23 18:50:43 -0400

    version 2.1

--- bat/busquedaProducto.bat
+++
@@ -1,5 +0,0 @@
-set projectPath=C:\Users\kiko\eclipse-workspace\HitesSelenium
-cd\
-cd %projectPath%
-set classpath=%projectPath%\bin;%projectPath%\lib\*;
-java org.testng.TestNG xml\Busqueda_producto.xml
